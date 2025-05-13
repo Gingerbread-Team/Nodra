bd20a84359280338048d19c24a8ba135f3f93035
Ahmed <ahmedsamirhelmy2003@gmail.com>
2025-05-13 20:32:37 +0300

    auth comments

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.nodra"
+        applicationId = "com.example.nodrah_project"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
